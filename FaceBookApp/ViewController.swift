e31e55ca1d46ae88612b925094c740fee499f37a
Vladyslav Vydovskyi <Vladyslav_Vydovskyi@epam.com>
2019-04-17 17:36:53 +0300

    Initial Commit

---
+++ FaceBookApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceBookApp
+//
+//  Created by Vladyslav Vydovskyi on 4/17/19.
+//  Copyright © 2019 Vladyslav Vydovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
